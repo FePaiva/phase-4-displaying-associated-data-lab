e59d851e645985f845118a120b749855a47fe6f8
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-02-02 09:00:35 -0800

    commit tree 810d036974437ef61ed2a8b6f1ce22a1165c1906

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+ def index
+    render json: Item.all, include: :user
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  # def index
+  #   render json: Users.all
+  # end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, include: :items
+  end
+
 end
